efc60343540c8d8df0403cb3ea668f2c5a0fe32f
anton-sales <anton.arus@gmail.com>
2018-04-25 17:32:28 +1200

    added div.green on style.css and grabbing divId two and linking it to style.css .green. Second circle is now green.

--- classes.js
+++ classes.js
@@ -3,14 +3,15 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
+function start() {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
+function one() {
   // First, we have to find the element:
   var one = document.getElementById('one')
 
@@ -19,6 +20,10 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two() {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
 
